320efe63269fd8c5f6bf39e615da1ebfc08ae957
unknown <edgarraegar@gmail.com>
2023-08-09 10:56:37 -0600

    Modified and some changes again

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -21,13 +21,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "5ix6f$06tx%ua@-+)8#^x2o$e7)yn$r9&(qleetsxo^$gh1bps"
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG')
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vodokeriadeluxe-production.up.railway.app']
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME:    
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 #PRUEBA DE LOGIN
 LOGIN_REDIRECT_URL = '/'
